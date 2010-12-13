1bf99f6ebc38cf782f9a220bb670d22819a5f7e1
Curtis (Programble) <programble@gmail.com>
2010-12-12 21:36:44 -0500

    Added a simple poll start

--- pollbot.rb
+++ pollbot.rb
@@ -152,6 +152,8 @@ class PollBot
     case command.strip
     when "ping"
       "pong"
+    when /^start ([^ ]+) or ([^ ]+)$/
+      start_poll("#{$1} or #{$2}", 5, nick, channel, [$1, $2])
     when /^start ([\d.]+) ([^:]+): (.+)/
       start_poll($2, $1.to_f, nick, channel, $3.split(";").map{|x| x.strip})
     when /^start ([^:]+): (.+)/
