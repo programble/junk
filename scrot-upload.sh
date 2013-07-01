d8298144a299870a88b79b43d34aecdd1e9c2d3a
Curtis McEnroe <programble@gmail.com>
2013-07-01 14:43:49 -0400

    Change screenshot links to programble.me

--- scrot-upload.sh
+++ scrot-upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scrot $@ -e 'scp -q $f quartz:/srv/webmount/programble/ss; mv $f ~/images/screenshots; echo -n "http://programble.cu.cc/ss/$n" | xclip -selection clipboard; notify-send -i up "Screenshot uploaded" "$n"'
+scrot $@ -e 'scp -q $f quartz:/srv/webmount/programble/ss; mv $f ~/images/screenshots; echo -n "http://programble.me/ss/$n" | xclip -selection clipboard; notify-send -i up "Screenshot uploaded" "$n"'
