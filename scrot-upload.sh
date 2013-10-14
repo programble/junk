5c758f910d2596022bf9783f09f00d0c9172f799
Curtis McEnroe <programble@gmail.com>
2013-10-14 00:44:45 -0400

    Simplify scrot-upload

--- scrot-upload.sh
+++ scrot-upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-scrot $@ -e 'scp -q $f quartz:/srv/webmount/programble/ss; mv $f ~/images/screenshots; echo -n "http://files.programble.me/ss/$n" | xclip -selection clipboard; notify-send -i up "Screenshot uploaded" "$n"'
+scrot $@ -e 'scp -q $f quartz:www/ss; mv $f ~/Pictures/screenshots; echo -n "http://files.programble.me/ss/$n" | xclip -selection clipboard; notify-send -i up "Screenshot uploaded" "$n"'
